f6deb2a2f8bf0e6db8c9bb4a65adac0234e785f9
huzaifazahoorchohan <huzaifazahoorpro@gmail.com>
2023-11-06 05:06:49 +0500

    new project
    typescript cli todoapp

---
+++ project03_todo_list/README.md
@@ -0,0 +1,19 @@
+# CLI ToDo App (Typescript)
+
+CLI-based TodoApp developed by Huzaifa Zahoor.
+
+This is a simple console-based ToDo app implemented in TypeScript. It allows you to manage your tasks through the command line interface.
+
+## Features
+
+- Add tasks to your ToDo list.
+- Update the tasks.
+- List all tasks.
+- Delete tasks from the list.
+
+
+## How to Use
+
+1. Install the Todo App using the following npm command:
+
+   npx zahoor_todo_list
---
+++ project03_todo_list/src/index.js
@@ -0,0 +1,59 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todoList = [];
+let todoQuit = true;
+const createTodo = async (todoArry) => {
+    do {
+        let questions = await inquirer.prompt({
+            type: "list",
+            message: "Please select your action:",
+            name: "select",
+            choices: ["ADD TODO", "UPDATE TODO", "READ TODOS", "DELETE TODO", "QUIT TODO"],
+        });
+        if (questions.select == "ADD TODO") {
+            let addTodo = await inquirer.prompt({
+                type: "input",
+                message: "Please write your todo:",
+                name: "todo",
+            });
+            todoArry.push(addTodo.todo);
+            console.log(todoArry);
+        }
+        if (questions.select == "UPDATE TODO") {
+            let selectedTodo = await inquirer.prompt({
+                type: "list",
+                message: "Which todo you want to update?",
+                name: "updateTodoItem",
+                choices: todoArry.map(todo => todo),
+            });
+            let updateTodo = await inquirer.prompt({
+                type: "input",
+                message: "Please update your todo:",
+                name: "updatedTodo",
+            });
+            let updatedTodoIndex = todoArry.indexOf(selectedTodo.updateTodoItem);
+            let newTodos = todoArry.filter(todo => todo !== selectedTodo.updateTodoItem);
+            newTodos.splice(updatedTodoIndex, 0, updateTodo.updatedTodo);
+            todoArry = newTodos;
+            console.log(todoArry);
+        }
+        if (questions.select == "READ TODOS") {
+            console.log(todoArry);
+        }
+        if (questions.select == "DELETE TODO") {
+            let deleteTodo = await inquirer.prompt({
+                type: "list",
+                message: "Which todo you want to delete?",
+                name: "deleteTodoItem",
+                choices: todoArry.map(todo => todo),
+            });
+            let newTodos = todoArry.filter(todo => todo !== deleteTodo.deleteTodoItem);
+            todoArry = [...newTodos];
+            console.log(todoArry);
+        }
+        if (questions.select == "QUIT TODO") {
+            todoQuit = false;
+        }
+    } while (todoQuit);
+};
+createTodo(todoList);
